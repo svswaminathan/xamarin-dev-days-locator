e104b12b4531a5328ab47b3616548c9f829d5494
z066157 <svswaminathan@gmail.com>
2016-08-19 21:25:26 +0530

    Added Android Project

---
+++ XamarinDevDaysFinder.Android/AgendaActivity.cs
@@ -0,0 +1,35 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using XamarinDevDaysFinder.Core;
+
+namespace XamarinDevDaysFinder.Droid
+{
+	[Activity(Label = "Agenda", MainLauncher = true)]
+	public class AgendaActivity : Activity
+	{
+		DataProvider provider;
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+			SetContentView(Resource.Layout.AgendaView);
+
+			provider = new DataProvider();
+			List<Session> sessions = provider.GetAgenda();
+			ListView myList = FindViewById<ListView>(Resource.Id.agendaListView);
+
+			myList.Adapter = new AgendaListViewAdapter(this, sessions);
+		
+		}
+	}
+}
+
---
+++ XamarinDevDaysFinder.Android/AgendaListViewAdapter.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+
+using Android.App;
+using Android.Views;
+using Android.Widget;
+using XamarinDevDaysFinder.Core;
+
+namespace XamarinDevDaysFinder.Droid
+{
+	public class AgendaListViewAdapter : BaseAdapter<Session>
+	{
+		List<Session> sessions = null;
+		Activity context;
+		public AgendaListViewAdapter(Activity context, List<Session> sessions)
+		{
+			this.context = context;
+			this.sessions = sessions;
+		}
+		public override long GetItemId(int position)
+		{
+			return position;
+		}
+		public override int Count
+		{
+			get
+			{
+				return sessions.Count;
+			}
+		}
+
+		public override View GetView(int position, View convertView, ViewGroup parent)
+		{
+			//throw new NotImplementedException();
+			var item = sessions[position];
+			View view = convertView;
+			if (view == null)
+			{
+				view = context.LayoutInflater.Inflate(Android.Resource.Layout.SimpleListItem2, null);
+				view.FindViewById<TextView>(Android.Resource.Id.Text1).Text = item.TopicName;
+				view.FindViewById<TextView>(Android.Resource.Id.Text2).Text = item.Timing;
+			}
+			return view;
+		}
+
+		public override Session this[int position]
+		{
+			get
+			{
+				return sessions[position];
+			}
+		}
+	}
+}
+
